2ac934d6cbdc8ac8a2cafeca33e8c1364d0add9b
thomasmeissner <thomas.meissner@sumup.com>
2023-06-08 21:05:57 +0200

    Fix test

--- bluecast/tests/test_load_for_production.py
+++ bluecast/tests/test_load_for_production.py
@@ -17,9 +17,7 @@ def test_load_for_production():
         pickle.dump(dummy_instance, file)
 
     # Call the function to load the saved instance
-    loaded_instance = load_for_production(
-        file_name=file_name, file_type=file_type
-    )
+    loaded_instance = load_for_production(file_name=file_name, file_type=file_type)
 
     # Check if the loaded instance is equal to the original dummy instance
     assert loaded_instance == dummy_instance
