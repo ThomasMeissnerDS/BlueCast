a7c840175939b754d1a41e25219f239f9c007384
thomasmeissner <thomas.meissner@sumup.com>
2023-06-08 20:51:27 +0200

    Add unit tests for blueprint laoding and saving

---
+++ bluecast/tests/test_load_for_production.py
@@ -0,0 +1,49 @@
+import os
+import pytest
+import pickle
+from bluecast.general_utils.general_utils import load_for_production
+
+
+def test_load_for_production():
+    # Define the file path, name, and type for the test file
+    file_path = "path/to/file/"
+    file_name = "test_instance"
+    file_type = ".dat"
+
+    # Create a dummy instance to save
+    dummy_instance = "test_data"
+
+    # Save the dummy instance to the file
+    with open(file_path + file_name + file_type, "wb") as file:
+        pickle.dump(dummy_instance, file)
+
+    # Call the function to load the saved instance
+    loaded_instance = load_for_production(file_path=file_path, file_name=file_name, file_type=file_type)
+
+    # Check if the loaded instance is equal to the original dummy instance
+    assert loaded_instance == dummy_instance
+
+    # Delete the test file
+    os.remove(file_path + file_name + file_type)
+
+
+def test_load_for_production_with_default_file_type():
+    # Define the file path and name for the test file
+    file_path = "path/to/file/"
+    file_name = "test_instance"
+
+    # Create a dummy instance to save
+    dummy_instance = "test_data"
+
+    # Save the dummy instance to the file with the default file type
+    with open(file_path + file_name + ".dat", "wb") as file:
+        pickle.dump(dummy_instance, file)
+
+    # Call the function to load the saved instance without specifying the file type
+    loaded_instance = load_for_production(file_path=file_path, file_name=file_name)
+
+    # Check if the loaded instance is equal to the original dummy instance
+    assert loaded_instance == dummy_instance
+
+    # Delete the test file
+    os.remove(file_path + file_name + ".dat")
---
+++ bluecast/tests/test_save_to_production.py
@@ -0,0 +1,38 @@
+import os
+import pytest
+import pickle
+from tempfile import TemporaryDirectory
+from bluecast.general_utils.general_utils import save_to_production
+
+
+class DummyClass:
+    def __init__(self, data):
+        self.data = data
+
+
+def test_save_to_production():
+    # Create a temporary directory to store the test file
+    with TemporaryDirectory() as temp_dir:
+        # Create a dummy instance
+        dummy_instance = DummyClass("test_data")
+
+        # Define the expected file path and name
+        file_path = temp_dir
+        file_name = "test_instance"
+        file_type = ".dat"
+
+        # Call the function to save the instance
+        save_to_production(dummy_instance, file_path=file_path, file_name=file_name, file_type=file_type)
+
+        # Construct the expected file path
+        expected_file_path = os.path.join(file_path, file_name + file_type)
+
+        # Check if the file was created
+        assert os.path.exists(expected_file_path)
+
+        # Load the saved instance
+        with open(expected_file_path, "rb") as file:
+            loaded_instance = pickle.load(file)
+
+        # Check if the loaded instance is equal to the original instance
+        assert loaded_instance.data == dummy_instance.data
