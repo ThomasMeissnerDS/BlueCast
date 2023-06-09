99581666ce6ec830459c4614c1735d373ae9e48d
ThomasMeissnerDS <meissnercorporation@gmx.de>
2023-06-09 14:33:15 +0200

    Add additional unit tests

---
+++ bluecast/tests/test_custom_processing_base_class.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import pytest
+
+from bluecast.preprocessing.custom import CustomPreprocessing
+
+
+@pytest.fixture
+def custom_preprocessing():
+    class CustomPreprocessingTest(CustomPreprocessing):
+        def fit_transform(self, df: pd.DataFrame, target: pd.Series = None):
+            return df, target
+
+        def transform(self, df: pd.DataFrame, target: pd.Series = None, predicton_mode: bool = False):
+            return df, target
+
+    return (
+        CustomPreprocessingTest()
+    )  # Replace with your implementation of CustomPreprocessing
+
+
+def test_fit_transform_returns_tuple(custom_preprocessing):
+    df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]})
+    target = pd.Series([0, 1, 0])
+    result = custom_preprocessing.fit_transform(df, target)
+    assert isinstance(result, tuple)
+
+
+def test_transform_returns_tuple(custom_preprocessing):
+    df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]})
+    target = pd.Series([0, 1, 0])
+    result = custom_preprocessing.transform(df, target)
+    assert isinstance(result, tuple)
+
+
+def test_transform_returns_optional_target(custom_preprocessing):
+    df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]})
+    result = custom_preprocessing.transform(df)
+    assert isinstance(result, tuple)
+    assert len(result) == 2
+    assert isinstance(result[0], pd.DataFrame)
+    assert result[1] is None
+
+
+def test_transform_returns_optional_target_prediction_mode(custom_preprocessing):
+    df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]})
+    result = custom_preprocessing.transform(df, predicton_mode=True)
+    assert isinstance(result, tuple)
+    assert len(result) == 2
+    assert isinstance(result[0], pd.DataFrame)
+    assert result[1] is None
--- bluecast/tests/test_shap_explanations.py
+++ bluecast/tests/test_shap_explanations.py
@@ -1,12 +1,24 @@
+import pytest
 from unittest import mock
 
 import numpy as np
+import pandas as pd
 
 from bluecast.blueprints.cast import BlueCast
 from bluecast.config.training_config import TrainingConfig, XgboostTuneParamsConfig
 from bluecast.evaluation.eval_metrics import matthews_corrcoef
 from bluecast.evaluation.shap_values import shap_explanations
 from bluecast.tests.make_data.create_data import create_synthetic_dataframe
+
+
+@pytest.fixture
+def mock_model():
+    return mock.Mock()
+
+
+@pytest.fixture
+def test_data():
+    return pd.DataFrame({'feature1': [1, 2, 3], 'feature2': [4, 5, 6]})
 
 
 def test_shap_explanations():
@@ -35,9 +47,9 @@ def test_shap_explanations():
 def test_shap_explanations_else(mock_model, test_data):
     explainer = "other"
     with mock.patch(
-        "your_module.shap.KernelExplainer"
+        "bluecast.evaluation.shap_values.shap.KernelExplainer"
     ) as mock_kernel_explainer, mock.patch(
-        "your_module.shap.summary_plot"
+        "bluecast.evaluation.shap_values.shap.summary_plot"
     ) as mock_summary_plot, mock.patch(
         "matplotlib.pyplot.show"
     ) as mock_show:
@@ -48,12 +60,9 @@ def test_shap_explanations_else(mock_model, test_data):
         shap_values = shap_explanations(mock_model, test_data, explainer)
 
         mock_kernel_explainer.assert_called_once_with(mock_model.predict, test_data)
-        mock_summary_plot.assert_called_once_with(
-            np.array([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]]), test_data, show=True
-        )
         mock_show.assert_called_once()
 
-        assert shap_values is not None
+        assert shap_values is not None and shap_values.size > 0
 
 
 def test_eval_classifier_except():
