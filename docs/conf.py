98b2e657d601a943de877ec60defe819751de830
thomasmeissner <thomas.meissner@sumup.com>
2023-06-09 07:18:09 +0200

    Update myst-parser

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = "0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst-parser", "sphinx.ext.autodoc"]
+extensions = ["myst_parser", "sphinx.ext.autodoc"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
