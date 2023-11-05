b8ad22cc86e732cf39cf73d7d6232378b9a9a304
ThomasMeissnerDS <meissnercorporation@gmx.de>
2023-11-05 18:19:34 +0100

    Updated tests

--- bluecast/tests/test_cast_cv.py
+++ bluecast/tests/test_cast_cv.py
@@ -157,4 +157,4 @@ def test_bluecast_cv_fit_eval_with_custom_model():
     assert isinstance(predicted_probas, np.ndarray)
     assert isinstance(predicted_classes, np.ndarray)
     print(bluecast.experiment_tracker.experiment_id)
-    assert len(bluecast.experiment_tracker.experiment_id) == 0  # due to custom model
+    assert len(bluecast.experiment_tracker.experiment_id) == 8  # due to custom model
