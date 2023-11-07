8f0bbd78a522565523097bf2337afd8bceab77e7
ThomasMeissnerDS <meissnercorporation@gmx.de>
2023-11-07 10:46:00 +0100

    Fix unit test

--- bluecast/tests/test_analyse.py
+++ bluecast/tests/test_analyse.py
@@ -103,7 +103,8 @@ def test_correlation_to_target(synthetic_train_test_data):
 def test_mutual_info_to_target(synthetic_train_test_data):
     mutual_info_to_target(
         synthetic_train_test_data[0].drop(
-            ["categorical_feature_1", "categorical_feature_2"], axis=1
+            ["categorical_feature_1", "categorical_feature_2", "datetime_feature"],
+            axis=1,
         ),
         "target",
     )
