3032f4aad303aaa77429b483051b616fe8c8c14b
ThomasMeissnerDS <meissnercorporation@gmx.de>
2024-05-28 10:51:51 +0200

    Add unit tests

--- bluecast/tests/test_cast_regression.py
+++ bluecast/tests/test_cast_regression.py
@@ -274,23 +274,23 @@ def test_bluecast_with_custom_model():
     # Create some sample data for testing
     x_train = pd.DataFrame(
         {
-            "feature1": [i for i in range(10)],
-            "feature2": [i for i in range(10)],
-            "feature3": [i for i in range(10)],
-            "feature4": [i for i in range(10)],
-            "feature5": [i for i in range(10)],
-            "feature6": [i for i in range(10)],
+            "feature1": [i for i in range(20)],
+            "feature2": [i for i in range(20)],
+            "feature3": [i for i in range(20)],
+            "feature4": [i for i in range(20)],
+            "feature5": [i for i in range(20)],
+            "feature6": [i for i in range(20)],
         }
     )
-    y_train = pd.Series([0, 1, 0, 1, 0, 1, 0, 1, 0, 1])
+    y_train = pd.Series([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1])
     x_test = pd.DataFrame(
         {
-            "feature1": [i for i in range(10)],
-            "feature2": [i for i in range(10)],
-            "feature3": [i for i in range(10)],
-            "feature4": [i for i in range(10)],
-            "feature5": [i for i in range(10)],
-            "feature6": [i for i in range(10)],
+            "feature1": [i for i in range(20)],
+            "feature2": [i for i in range(20)],
+            "feature3": [i for i in range(20)],
+            "feature4": [i for i in range(20)],
+            "feature5": [i for i in range(20)],
+            "feature6": [i for i in range(20)],
         }
     )
 
