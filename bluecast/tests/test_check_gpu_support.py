d9c063470049946702eb7411e8ec2e8ea7843d97
ThomasMeissnerDS <meissnercorporation@gmx.de>
2024-06-22 10:03:09 +0200

    Optimize unit test

--- bluecast/tests/test_check_gpu_support.py
+++ bluecast/tests/test_check_gpu_support.py
@@ -6,17 +6,21 @@ import xgboost as xgb
 from bluecast.general_utils.general_utils import check_gpu_support
 
 
-def test_check_gpu_support_cuda(monkeypatch):
+def test_check_gpu_support(monkeypatch):
     """
-    Test check_gpu_support function for CUDA support.
+    Test check_gpu_support function.
+
+    We use the monkeypatch fixture provided by pytest to mock the xgb.train function and numpy's random functions.
+    The mock_train function is defined as a replacement for xgb.train and asserts that the expected parameters are
+    passed and the labels are set correctly. We then patch numpy's rand function to return a predefined array for data
+    and randint function to return a predefined array for labels.
+
+    Finally, we call check_gpu_support and assert that it returns the expected output, which in this case is "gpu_hist".
     """
 
-    def mock_train(params, d_train, num_boost_round):
-        assert params == {
-            "device": "cuda",
-            "tree_method": "gpu",
-            "predictor": "gpu_predictor",
-        }
+    def mock_train(params, d_train):
+        assert params == {"device": "cuda"}
+        assert np.array_equal(d_train.get_label(), np.array([0, 1, 0, 1, 0]))
         return None
 
     monkeypatch.setattr(xgb, "train", mock_train)
@@ -28,104 +32,20 @@ def test_check_gpu_support_cuda(monkeypatch):
         ),
     )
     monkeypatch.setattr(
-        np.random, "randint", lambda low, high, size: np.array([0, 1, 0, 1, 0])
-    )
-
-    assert check_gpu_support() == {
-        "device": "cuda",
-        "tree_method": "gpu",
-        "predictor": "gpu_predictor",
-    }
-
-
-def test_check_gpu_support_gpu_hist(monkeypatch):
-    """
-    Test check_gpu_support function for GPU hist support.
-    """
-
-    def mock_train(params, d_train, num_boost_round):
-        assert params == {
-            "tree_method": "gpu",
-        }
-        return None
-
-    # Trigger an exception for the first try block
-    def mock_train_fail(params, d_train, num_boost_round):
-        raise Exception("GPU not supported")
-
-    monkeypatch.setattr(xgb, "train", mock_train_fail)
-    monkeypatch.setattr(xgb, "train", mock_train, raising=False)
-    monkeypatch.setattr(
-        np.random,
-        "rand",
-        lambda *args: np.array(
-            [[0.1, 0.2], [0.3, 0.4], [0.5, 0.6], [0.7, 0.8], [0.9, 0.1]]
-        ),
-    )
-    monkeypatch.setattr(
-        np.random, "randint", lambda low, high, size: np.array([0, 1, 0, 1, 0])
-    )
-
-    assert check_gpu_support() == {"tree_method": "gpu"}
-
-
-def test_check_gpu_support_cpu(monkeypatch):
-    """
-    Test check_gpu_support function for CPU support.
-    """
-
-    def mock_train(params, d_train, num_boost_round):
-        raise Exception("GPU not supported")
-
-    monkeypatch.setattr(xgb, "train", mock_train)
-    monkeypatch.setattr(
-        np.random,
-        "rand",
-        lambda *args: np.array(
-            [[0.1, 0.2], [0.3, 0.4], [0.5, 0.6], [0.7, 0.8], [0.9, 0.1]]
-        ),
-    )
-    monkeypatch.setattr(
-        np.random, "randint", lambda low, high, size: np.array([0, 1, 0, 1, 0])
+        np.random, "randint", lambda low, size: np.array([0, 1, 0, 1, 0])
     )
 
     assert check_gpu_support() == {"tree_method": "hist", "device": "cpu"}
 
 
-def test_check_gpu_support_logging(caplog, monkeypatch):
+def test_check_gpu_support_logging(caplog):
     with caplog.at_level(logging.INFO):
-
-        def mock_train(params, d_train, num_boost_round):
-            if params.get("device") == "cuda":
-                return None
-            elif params.get("tree_method") == "gpu":
-                return None
-            raise Exception("GPU not supported")
-
-        monkeypatch.setattr(xgb, "train", mock_train)
-        monkeypatch.setattr(
-            np.random,
-            "rand",
-            lambda *args: np.array(
-                [[0.1, 0.2], [0.3, 0.4], [0.5, 0.6], [0.7, 0.8], [0.9, 0.1]]
-            ),
-        )
-        monkeypatch.setattr(
-            np.random, "randint", lambda low, high, size: np.array([0, 1, 0, 1, 0])
-        )
-
         params = check_gpu_support()
 
         # Verify the logging output
         assert "Start checking if GPU is available for usage." in caplog.text
 
-        if params.get("device") == "cuda":
-            assert "Xgboost uses GPU." in caplog.text
-            assert (
-                "Can use {'device': 'cuda', 'tree_method': 'gpu', 'predictor': 'gpu_predictor'} for Xgboost"
-                in caplog.text
-            )
-        elif params.get("tree_method") == "gpu":
+        if "gpu" in params["tree_method"]:
             assert "Xgboost uses GPU." in caplog.text
             assert "Can use {'tree_method': 'gpu'}." in caplog.text
         else:
@@ -134,3 +54,15 @@ def test_check_gpu_support_logging(caplog, monkeypatch):
                 "Can use {'tree_method': 'hist', 'device': 'cpu'} for Xgboost"
                 in caplog.text
             )
+
+        # Check the returned params are correctly logged
+        if params["device"] == "cuda":
+            assert params == {
+                "device": "cuda",
+                "tree_method": "gpu",
+                "predictor": "gpu_predictor",
+            }
+        elif "gpu" in params["tree_method"]:
+            assert params == {"tree_method": "gpu"}
+        else:
+            assert params == {"tree_method": "hist", "device": "cpu"}
