347e8c11aa0bc69b20b0884001437aec2d75c8d5
ThomasMeissnerDS <meissnercorporation@gmx.de>
2024-07-23 14:15:45 +0200

    Add ModelMatchMaker

---
+++ bluecast/blueprints/orchestration.py
@@ -0,0 +1,72 @@
+"""Module containing model orchestration tools."""
+
+from typing import List, Optional, Tuple, Union
+
+import numpy as np
+import pandas as pd
+
+from bluecast.blueprints.cast import BlueCast
+from bluecast.blueprints.cast_cv import BlueCastCV
+from bluecast.blueprints.cast_cv_regression import BlueCastCVRegression
+from bluecast.blueprints.cast_regression import BlueCastRegression
+from bluecast.monitoring.data_monitoring import DataDrift
+
+
+class ModelMatchMaker:
+    """
+    Matching the incoming data with the best model based on the adversarial validation score.
+    """
+
+    def __init__(self):
+        self.bluecast_instances = []
+        self.training_datasets = []
+
+    def append_model_and_dataset(
+        self,
+        bluecast_instance: Union[
+            BlueCast, BlueCastRegression, BlueCastCV, BlueCastCVRegression
+        ],
+        df: pd.DataFrame,
+    ):
+        """
+        Append the model and the dataset to the matchmaker.
+
+        :param bluecast_instance: The BlueCast instance to append.
+        :param df: The dataset to append.
+        """
+        self.bluecast_instances.append(bluecast_instance)
+        self.training_datasets.append(df)
+
+    def find_best_match(
+        self, df: pd.DataFrame, use_cols: List[Union[int, float, str]], threshold: float
+    ) -> Tuple[
+        Optional[Union[BlueCast, BlueCastRegression, BlueCastCV, BlueCastCVRegression]],
+        Optional[pd.DataFrame],
+    ]:
+        """
+        Find the best match based on the adversarial validation score.
+        :param df: Dataset to match.
+        :param use_cols: Columns to use for the adversarial validation. Numerical columns are allowed only.
+        :param threshold: Maximum threshold for the adversarial validation score. If no dataset reaches this threshold,
+            (None, None) is returned.
+        :return: If a match is found, the BlueCast instance and the dataset are returned. Otherwise, (None, None) is
+            returned.
+        """
+        best_score = np.inf
+        best_idx = None
+
+        for idx in range(len(self.bluecast_instances)):
+            data_drift_checker = DataDrift()
+            auc_score = data_drift_checker.adversarial_validation(
+                self.training_datasets[idx].loc[:, use_cols], df.loc[:, use_cols]
+            )
+            if auc_score < best_score and auc_score < threshold:
+                print(f"Found best match using idx {idx} with AUC score of {auc_score}")
+                best_score = auc_score
+                best_idx = idx
+
+        if best_idx:
+            return self.bluecast_instances[best_idx], self.training_datasets[best_idx]
+        else:
+            print("No training dataset has reached the threshold criterium.")
+            return None, None
