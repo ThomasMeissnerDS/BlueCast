1834674524b08ced7a727bd42963de9f90db7f74
ThomasMeissnerDS <meissnercorporation@gmx.de>
2024-10-23 07:18:59 +0200

    Fix index

--- bluecast/preprocessing/target_encoding.py
+++ bluecast/preprocessing/target_encoding.py
@@ -34,7 +34,7 @@ class BinaryClassTargetEncoder:
                 "Encoding string targets temporarily for binary classification."
             )
             label_encoder = LabelEncoder()
-            y = label_encoder.fit_transform(y)
+            y = pd.Series(label_encoder.fit_transform(y))
             y.index = x.index
 
         smoothing = np.max([np.log10(len(x.index)) * 5, 10])
