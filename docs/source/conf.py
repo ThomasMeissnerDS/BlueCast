7a7cdd7cdd2d3cd93fc7d84f8f3ee7ff8d96f6fb
ThomasMeissnerDS <meissnercorporation@gmx.de>
2025-01-14 11:48:11 +0100

    Release version 2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ for x in os.walk("../../src"):
 project = "BlueCast"
 copyright = "2024, Thomas Meißner"
 author = "Thomas Meißner"
-release = "1.6.3"
+release = "2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
