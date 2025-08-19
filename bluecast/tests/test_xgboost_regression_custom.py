efce1430d1b8657386a41f93f1fd21d490bcdd99
thomasmeissner <thomas.meissner@sumup.com>
2025-08-19 14:18:37 +0200

    Add Xgboost unit tests

---
+++ bluecast/tests/test_xgboost_regression_custom.py
@@ -0,0 +1,139 @@
+import numpy as np
+import pandas as pd
+
+from bluecast.blueprints.cast_regression import BlueCastRegression
+from bluecast.config.training_config import TrainingConfig, XgboostTuneParamsRegressionConfig
+from bluecast.ml_modelling.xgboost_regression import XgboostModelRegression
+from bluecast.preprocessing.custom import CustomPreprocessing
+from typing import Optional, Tuple
+
+
+def test_bluecast_regression_with_custom_xgboost_no_tuning():
+	train_config = TrainingConfig()
+	train_config.hyperparameter_tuning_rounds = 5
+	train_config.hypertuning_cv_folds = 2
+	train_config.autotune_model = False
+
+	xgboost_param_config = XgboostTuneParamsRegressionConfig()
+	xgboost_param_config.steps_min = 2
+	xgboost_param_config.steps_max = 100
+	xgboost_param_config.max_depth_max = 3
+
+	class MyCustomLastMilePreprocessing(CustomPreprocessing):
+		def custom_function(self, df: pd.DataFrame) -> pd.DataFrame:
+			df["custom_col"] = 5
+			return df
+
+		def fit_transform(
+			self, df: pd.DataFrame, target: pd.Series
+		) -> Tuple[pd.DataFrame, pd.Series]:
+			df = self.custom_function(df)
+			return df, target
+
+		def transform(
+			self,
+			df: pd.DataFrame,
+			target: Optional[pd.Series] = None,
+			predicton_mode: bool = False,
+		) -> Tuple[pd.DataFrame, Optional[pd.Series]]:
+			df = self.custom_function(df)
+			return df, target
+
+	bluecast = BlueCastRegression(
+		class_problem="regression",
+		ml_model=XgboostModelRegression(
+			class_problem="regression",
+			conf_training=train_config,
+			conf_xgboost=xgboost_param_config,
+		),
+		conf_xgboost=xgboost_param_config,
+		conf_training=train_config,
+		custom_last_mile_computation=MyCustomLastMilePreprocessing(),
+	)
+
+	x_train = pd.DataFrame(
+		{
+			"feature1": [i for i in range(20)],
+			"feature2": [i for i in range(20)],
+			"feature3": [i for i in range(20)],
+			"feature4": [i for i in range(20)],
+			"feature5": [i for i in range(20)],
+			"feature6": [i for i in range(20)],
+		}
+	)
+	y_train = pd.Series([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1])
+	x_test = pd.DataFrame(
+		{
+			"feature1": [i for i in range(10)],
+			"feature2": [i for i in range(10)],
+			"feature3": [i for i in range(10)],
+			"feature4": [i for i in range(10)],
+			"feature5": [i for i in range(10)],
+			"feature6": [i for i in range(10)],
+		}
+	)
+
+	x_train["target"] = y_train
+
+	bluecast.fit(x_train, "target")
+
+	predicted_values = bluecast.predict(x_test)
+
+	assert isinstance(predicted_values, np.ndarray)
+	assert len(bluecast.experiment_tracker.experiment_id) == 0
+
+
+
+def test_bluecast_regression_with_custom_xgboost_with_tuning():
+	train_config = TrainingConfig()
+	train_config.hyperparameter_tuning_rounds = 5
+	train_config.hypertuning_cv_folds = 2
+	train_config.autotune_model = True
+	train_config.plot_hyperparameter_tuning_overview = False
+
+	xgboost_param_config = XgboostTuneParamsRegressionConfig()
+	xgboost_param_config.steps_min = 2
+	xgboost_param_config.steps_max = 100
+	xgboost_param_config.max_depth_max = 3
+
+	bluecast = BlueCastRegression(
+		class_problem="regression",
+		ml_model=XgboostModelRegression(
+			class_problem="regression",
+			conf_training=train_config,
+			conf_xgboost=xgboost_param_config,
+		),
+		conf_xgboost=xgboost_param_config,
+		conf_training=train_config,
+	)
+
+	x_train = pd.DataFrame(
+		{
+			"feature1": [i for i in range(20)],
+			"feature2": [i for i in range(20)],
+			"feature3": [i for i in range(20)],
+			"feature4": [i for i in range(20)],
+			"feature5": [i for i in range(20)],
+			"feature6": [i for i in range(20)],
+		}
+	)
+	y_train = pd.Series([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1])
+	x_test = pd.DataFrame(
+		{
+			"feature1": [i for i in range(10)],
+			"feature2": [i for i in range(10)],
+			"feature3": [i for i in range(10)],
+			"feature4": [i for i in range(10)],
+			"feature5": [i for i in range(10)],
+			"feature6": [i for i in range(10)],
+		}
+	)
+
+	x_train["target"] = y_train
+
+	bluecast.fit(x_train, "target")
+
+	predicted_values = bluecast.predict(x_test)
+
+	assert isinstance(predicted_values, np.ndarray)
+	assert len(bluecast.experiment_tracker.experiment_id) == 5 
